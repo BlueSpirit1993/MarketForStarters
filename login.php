a66d25b9517c7e702ebfaf2a0cb98c7c40a7fd7c
BlueSpirit1993 <69617731+BlueSpirit1993@users.noreply.github.com>
2020-08-21 21:08:33 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,13 +4,65 @@
 //Date Modified: 07/19/2020
 
 <?php
+echo "<h1>Login Here</h1>";
+if(isset($_POST['login']))
+{
+	$uid = $_POST['uid'];
+	$pwd = $_POST['pwd'];
+	
+	$conn = mysqli_connect("localhost","root","" );
+    if(!$conn){ die("connection failed:".mysqli_connect_errno()."=".mysqli_connect_error());
+    }
+	mysqli_select_db($conn,"users");
+	
+	$checkQuery = "SELECT * FROM userlist WHERE uId = '$uid' and password = '$pwd'";
+	$results = mysqli_query($conn,$checkQuery);
+	
+	$user = mysqli_fetch_array($results);
+	if(($user['uId']==$uid)&&$user['password']= $pwd)
+	{
+		session_start();
+		if($user['type']==1)
+		{
+			$_SESSION['user']=$user['uId'];
+			header("location:adminmain.php");
+		}
+		if($user['type']==0)
+		{
+			$_SESSION['user']=$user['uId'];
+			header("location:usermain.php");
+		}
+		if($user['type']==2)
+		{
+			session_destroy();
+			echo "<div><font style='color:red'>Your account has been suspended. Please contact your administrator</font></div>";
+		}
+	}
+	else
+	{
+		echo "<font color = 'red'>Login failed</font><br>";
+	}
+}
+
+
 
 ?>
-<!DOCTYPE html>
 <html>
-<head>
-  <title>Login</title>
-</head>
-<body>
+<body style="background-color:lightgrey;">
+
+<form action = "login.php" method = "post">
+
+User ID:<input type = 'text' name = 'uid'><br>
+Password:<input type = 'text' name = 'pwd'><br>
+<input type = 'submit' name = 'login' value = 'Login'><br>
+<a href = "register.php">Click here to register if you don't have an account</a>
+</form>
+
+
+
+
+
+
+
 </body>
 </html>
